3ce5a6b06495d36910d1be9a66fa4a7eae37d6a1
Andrew Thayer <athayer2@cvtc.edu>
2023-09-28 14:17:56 -0500

    updated js file

--- public/darkMode.js
+++ public/darkMode.js
@@ -1,5 +1,5 @@
 const styleToggle = document.getElementById('button')
-let bodyRef = document.getElementById('body')
+const bodyRef = document.getElementById('body')
 
 styleToggle.addEventListener('click',
   function () {
