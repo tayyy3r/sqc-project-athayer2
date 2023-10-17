b617782f933b2880b95c2e704a67356f6f143fcd
Andrew Thayer <athayer2@cvtc.edu>
2023-10-17 12:04:53 -0500

    updated html to sql with bug fixes

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 // Dependencies ////////////////////////////////////////////
+import 'dotenv/config'
 import express from 'express'
+import pkg from 'pg'
+const { Pool } = pkg
 
 // Configuration ///////////////////////////////////////////
 const PORT = process.env.PORT || 5163
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
+// Query functions /////////////////////////////////////////
+
 
 // Web server setup ////////////////////////////////////////
 const app = express()
